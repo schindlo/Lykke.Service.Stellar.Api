a9e967e6d7f7588034af6b15c7e7d0220097e44a
mrunibe <26478127+mrunibe@users.noreply.github.com>
2018-01-30 10:13:51 +0100

    add capabilities api

---
+++ src/Lykke.Service.Stellar.Api/Controllers/CapabilitiesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Lykke.Service.BlockchainApi.Contract.Common;
+
+namespace Lykke.Service.Stellar.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CapabilitiesController : Controller
+    {
+        
+        /// <summary>
+        /// Indicate if optional operations are supported
+        /// </summary>
+        [HttpGet]
+        [SwaggerOperation("capabilities")]
+        [ProducesResponseType(typeof(CapabilitiesResponse), (int)HttpStatusCode.OK)]
+        public IActionResult Get()
+        {
+            return Ok(new CapabilitiesResponse
+            {
+                IsTransactionsRebuildingSupported = false,
+                AreManyInputsSupported = false,
+                AreManyOutputsSupported = false
+            });
+        }
+
+    }
+}
