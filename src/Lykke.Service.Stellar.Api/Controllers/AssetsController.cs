3da86587e45e8d8e9ed33bf61896c43916503870
Oliver Schindler <oliver@schindler.gdn>
2018-03-12 19:48:54 +0100

    fix asset controller validation

--- src/Lykke.Service.Stellar.Api/Controllers/AssetsController.cs
+++ src/Lykke.Service.Stellar.Api/Controllers/AssetsController.cs
@@ -6,6 +6,7 @@ using Lykke.Service.Stellar.Api.Core.Domain;
 using Lykke.Service.Stellar.Api.Helpers;
 using Lykke.Service.BlockchainApi.Contract;
 using Lykke.Service.BlockchainApi.Contract.Assets;
+using Lykke.Common.Api.Contract.Responses;
 
 namespace Lykke.Service.Stellar.Api.Controllers
 {
@@ -14,21 +15,32 @@ namespace Lykke.Service.Stellar.Api.Controllers
     {
         [HttpGet]
         [SwaggerOperation("assets")]
-        public PaginationResponse<AssetResponse> Get([Required, FromQuery] int take, [FromQuery] string continuation)
+        [ProducesResponseType(typeof(PaginationResponse<AssetResponse>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
+        public IActionResult Get([Required, FromQuery] int take, [FromQuery] string continuation)
         {
-            var assets = new AssetResponse[] { Asset.Stellar.ToAssetResponse() };
+            if (take < 1)
+            {
+                return BadRequest(ErrorResponse.Create("Invalid parameter").AddModelError("take", "Must be positive non zero integer"));
+            }
+            if (!string.IsNullOrEmpty(continuation))
+            {
+                return BadRequest(ErrorResponse.Create("Invalid parameter").AddModelError("continuation", "Must be valid continuation token"));
+            }
 
-            return PaginationResponse.From("", assets);
+            var assets = new AssetResponse[] { Asset.Stellar.ToAssetResponse() };
+            return Ok(PaginationResponse.From("", assets));
         }
 
         [HttpGet("{assetId}")]
         [SwaggerOperation("assets/")]
         [ProducesResponseType(typeof(AssetResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
         public IActionResult GetAsset([Required] string assetId)
         {
             if (Asset.Stellar.Id != assetId)
             {
-                return NotFound();
+                return NoContent();
             }
 
             return Ok(Asset.Stellar.ToAssetResponse());
