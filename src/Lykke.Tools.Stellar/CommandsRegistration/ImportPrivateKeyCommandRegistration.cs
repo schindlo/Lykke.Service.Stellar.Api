36d417f3edd7e00a126f810796314043b8bb14f0
AleksandrBelousov <tortyt1@gmail.com>
2019-02-12 19:05:43 +0400

    Added feature to import private key via console command. See instruction in tool folder

---
+++ src/Lykke.Tools.Stellar/CommandsRegistration/ImportPrivateKeyCommandRegistration.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+using Lykke.Tools.Stellar.Commands;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Lykke.Tools.Stellar.CommandsRegistration
+{
+    [CommandRegistration("import-private-key")]
+    public class ImportPrivateKeyCommandRegistration : ICommandRegistration
+    {
+        private readonly CommandFactory _factory;
+
+        public ImportPrivateKeyCommandRegistration(CommandFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public void StartExecution(CommandLineApplication lineApplication)
+        {
+            lineApplication.Description = "This is the description for scan-deposits-withdraw.";
+            lineApplication.HelpOption("-?|-h|--help");
+
+            var signFacadeUrl = lineApplication.Option("-sfu|--sign-facade-url <optionvalue>",
+                "Sign Facade Url",
+                CommandOptionType.SingleValue);
+
+            var apiKey = lineApplication.Option("-ak|--api-key <optionvalue>",
+                "ApiKey With Import Access",
+                CommandOptionType.SingleValue);
+
+            var blockchainType = lineApplication.Option("-bt|--blockchain-type <optionvalue>",
+                "BlockchainType(Stellar, Kin)",
+                CommandOptionType.SingleValue);
+
+            lineApplication.OnExecute(async () =>
+            {
+                var command = _factory.CreateCommand((helper) => new ImportPrivateKeyCommand(
+                    signFacadeUrl.Value(),
+                    apiKey.Value(),
+                    blockchainType.Value()));
+
+                return await command.ExecuteAsync();
+            });
+        }
+    }
+}
+
